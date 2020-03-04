33230d76bea2cea4af3ab87e75e3a568d5312c25
Harry Messenger <harrymessenger@Harrys-MacBook-Pro.local>
2020-03-04 12:17:11 +0000

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function test()
+    {
+        dd('we have reached test');
+    }
+    public function name(User $name)
+    {
+        $all_users = User::all();
+        return view('hello',compact('name','all_users'));
+    }
+}
